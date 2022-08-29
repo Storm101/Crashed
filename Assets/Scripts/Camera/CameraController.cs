2a4e079719daf72da33e6f367f687cf6b868dc4f
DonElf <truedonelf@gmail.com>
2022-08-29 19:46:57 +1000

    Added new CameraController
    Added a new camera controller capable of head bob, footstep sfx, and better customization.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CameraController : MonoBehaviour
+{
+    [TextArea]
+    [SerializeField]
+    [Tooltip("ReadMe")]
+    private string ReadMe = "Requires an Audio Source to work\nApply this script to the main camera, and should be child of a gameobject holding the 'PlayerController' script.";
+
+    [SerializeField]
+    [Tooltip("Sensitivity of the mouse camera movements. Recommended around 2")]
+    private float MouseSensitivity;
+
+    [Header("- Footsteps -")]
+    [SerializeField]
+    [Tooltip("Enable footsteps while walking?")]
+    private bool Footsteps = false;
+    [SerializeField]
+    [Tooltip("The audio to play when the player has taken a step")]
+    private AudioClip[] audioClips;
+
+    [Header("- Head Bob -")]
+    [SerializeField]
+    [Tooltip("Enable head bobbing while walking?")]
+    private bool headBob = false;
+    [SerializeField]
+    [Range(-1.5f, 0)]
+    [Tooltip("The maximum distance the head may bob downwards. Recommended at around -0.95")]
+    private float MinHeadBob;
+    [SerializeField]
+    [Tooltip("The maximum distance the head may bob upwards. Recommended at around 0.9")]
+    [Range(0, 1.5f)]
+    private float MaxHeadBob;
+    [SerializeField]
+    [Tooltip("Total intensisty of the head bob. Recommended at around 0.1")]
+    [Range(0, 0.5f)]
+    private float HeadBobIntensity;
+
+    private AudioSource audioSource;
+    private bool PlayAgain = false;
+    private float StartPos;
+    private float HeadBobTimer;
+    private float MouseYSum;
+
+    private void Awake() {
+        StartPos = transform.localPosition.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity;
+            float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity;
+            if (MouseY + MouseYSum >= 75) {
+                transform.localEulerAngles = new Vector3(-75, transform.localEulerAngles.y, transform.localEulerAngles.z);
+                MouseY = 0;
+                MouseYSum = 75;
+            }
+            else if (MouseY + MouseYSum <= -75) {
+                transform.localEulerAngles = new Vector3(75, transform.localEulerAngles.y, transform.localEulerAngles.z);
+                MouseY = 0;
+                MouseYSum = -75;
+            }
+            else if (Cursor.lockState == CursorLockMode.Locked) {
+                MouseYSum += MouseY;
+            }
+
+            if (Cursor.lockState == CursorLockMode.Locked) {
+                transform.parent.Rotate(Vector3.up, MouseX, Space.World);
+                transform.Rotate(transform.parent.right, -MouseY, Space.World);
+
+
+            }
+            else if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q) && Cursor.lockState == CursorLockMode.Locked) {
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
+
+        if ((Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0) && GameManager.Instance.Grounded) {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                HeadBob(15);
+            }
+            else {
+                HeadBob(10);
+            }
+        }
+    }
+
+    private void HeadBob(float Speed) {
+        HeadBobTimer += Time.deltaTime * Speed;
+
+        if (headBob) { 
+            transform.localPosition = new Vector3(transform.localPosition.x, StartPos + Mathf.Sin(HeadBobTimer) * HeadBobIntensity, transform.localPosition.z);
+        }
+
+        if (Footsteps) {
+            if (Mathf.Sin(HeadBobTimer) < MinHeadBob && PlayAgain) {
+                audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+                audioSource.Play();
+                PlayAgain = false;
+            }
+            else if (Mathf.Sin(HeadBobTimer) > MaxHeadBob && !PlayAgain) {
+                PlayAgain = true;
+            }
+        }
+    }
+
+
+    //Functions for options menu
+
+    public void UpdateSensitivity(float sensivity) {
+        MouseSensitivity = sensivity;
+    }
+
+    public void HeadBobSwitch(bool toggle) {
+        headBob = toggle;
+    }
+
+    public void UpdateHeadBobIntensity(float intensity) {
+        HeadBobIntensity = intensity;
+    }
+}
--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -119,7 +119,7 @@ namespace StarterAssets
 
 		private void LateUpdate()
 		{
-			CameraRotation();
+			//CameraRotation();
 		}
 
 		private void GroundedCheck()
@@ -127,6 +127,7 @@ namespace StarterAssets
 			// set sphere position, with offset
 			Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
 			Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
+			GameManager.Instance.Grounded = Grounded;
 		}
 
 		private void CameraRotation()

a6d697262ec97f46b01ecf010b0e8e1d57f0a45e
Storm101 <trimoore5@gmail.com>
2022-09-12 20:14:28 +1000

    Added aiming

--- Assets/Scripts/Managers/WeaponManager.cs
+++ Assets/Scripts/Managers/WeaponManager.cs
@@ -48,12 +48,12 @@ public class WeaponManager : MonoBehaviour
     }
 
     //Aiming function
-    public void Aim(GameObject currentWeapon, Transform weaponPosition, Transform adsPosition, bool isAiming)
+    public void Aim(GameObject currentWeapon, Transform weaponPosition, Transform adsPosition, Weapons currentWeaponData, bool isAiming)
     {
         if (isAiming)
-            currentWeapon.transform.position = Vector3.Lerp(currentWeapon.transform.position, adsPosition.localPosition, Time.deltaTime);
+            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, adsPosition.localPosition, currentWeaponData.adsSpeed * Time.deltaTime);
         else
-            currentWeapon.transform.position = Vector3.Lerp(currentWeapon.transform.position, weaponPosition.localPosition, Time.deltaTime);
+            currentWeapon.transform.localPosition = Vector3.Lerp(currentWeapon.transform.localPosition, weaponPosition.localPosition, currentWeaponData.adsSpeed * Time.deltaTime);
     }
 
     //Swap weapons function
@@ -65,135 +65,138 @@ public class WeaponManager : MonoBehaviour
     //Cooldown functions
     public void Cooldowns(WeaponClasses weaponList)
     {
-        foreach (Weapons weapon in weaponList.primaryWeapons)
-        {
-            //If the weapon is cooling and is not recharging, decrease the cooldown timer until it is less than or equal to 0
-            if (weapon.isCooling && !weapon.recharging)
+        if (weaponList.primaryWeapons != null)
+            foreach (Weapons weapon in weaponList.primaryWeapons)
             {
-                if (weapon.coolingCDTimer <= 0)
+                //If the weapon is cooling and is not recharging, decrease the cooldown timer until it is less than or equal to 0
+                if (weapon.isCooling && !weapon.recharging)
                 {
-                    weapon.coolingCDTimer = 0;
-                    weapon.isCooling = false;
+                    if (weapon.coolingCDTimer <= 0)
+                    {
+                        weapon.coolingCDTimer = 0;
+                        weapon.isCooling = false;
+                    }
+                    else
+                        weapon.coolingCDTimer -= Time.deltaTime;
                 }
-                else
+                //If the weapon is recharging, drecrease the cooldown timer faster until it is less than or equal to 0
+                else if (weapon.isCooling && weapon.recharging)
+                {
+                    if (weapon.coolingCDTimer <= 0)
+                    {
+                        weapon.coolingCDTimer = 0;
+                        weapon.isCooling = false;
+                    }
+                    else
+                        weapon.coolingCDTimer -= Time.deltaTime * weapon.rechargingSpeed;
+                }
+                //If the weapon is not cooling but its cooldownCDTimer is greater tahn 0, decrease teh cooldown timer by Time.deltaTime
+                else if (weapon.coolingCDTimer > 0)
                     weapon.coolingCDTimer -= Time.deltaTime;
-            }
-            //If the weapon is recharging, drecrease the cooldown timer faster until it is less than or equal to 0
-            else if (weapon.isCooling && weapon.recharging)
-            {
-                if (weapon.coolingCDTimer <= 0)
-                {
-                    weapon.coolingCDTimer = 0;
-                    weapon.isCooling = false;
-                }
-                else
-                    weapon.coolingCDTimer -= Time.deltaTime * weapon.rechargingSpeed;
-            }
-            //If the weapon is not cooling but its cooldownCDTimer is greater tahn 0, decrease teh cooldown timer by Time.deltaTime
-            else if (weapon.coolingCDTimer > 0)
-                weapon.coolingCDTimer -= Time.deltaTime;
-            //If the weapon's coolingCDTimer is greater than or equal to the coolingCooldown, set the weapon cooling to true
-            if (weapon.coolingCDTimer >= weapon.coolingCooldown)
-                weapon.isCooling = true;
+                //If the weapon's coolingCDTimer is greater than or equal to the coolingCooldown, set the weapon cooling to true
+                if (weapon.coolingCDTimer >= weapon.coolingCooldown)
+                    weapon.isCooling = true;
 
-            //If the weapon has shot, increase the rate of fire cooldown until it is greater tahn or equal to the cooldown length
-            if (weapon.hasShot)
-            {
-                if (weapon.rateOfFireCDTimer >= weapon.rateOfFire)
+                //If the weapon has shot, increase the rate of fire cooldown until it is greater tahn or equal to the cooldown length
+                if (weapon.hasShot)
                 {
-                    weapon.rateOfFireCDTimer = 0;
-                    weapon.hasShot = false;
+                    if (weapon.rateOfFireCDTimer >= weapon.rateOfFire)
+                    {
+                        weapon.rateOfFireCDTimer = 0;
+                        weapon.hasShot = false;
+                    }
+                    else
+                        weapon.rateOfFireCDTimer += Time.deltaTime;
                 }
-                else
-                    weapon.rateOfFireCDTimer += Time.deltaTime;
             }
-        }
-        foreach (Weapons weapon in weaponList.secondaryWeapons)
-        {
-            //If the weapon is cooling and is not recharging, decrease the cooldown timer until it is less than or equal to 0
-            if (weapon.isCooling && !weapon.recharging)
+        if (weaponList.secondaryWeapons != null)
+            foreach (Weapons weapon in weaponList.secondaryWeapons)
             {
-                if (weapon.coolingCDTimer <= 0)
+                //If the weapon is cooling and is not recharging, decrease the cooldown timer until it is less than or equal to 0
+                if (weapon.isCooling && !weapon.recharging)
                 {
-                    weapon.coolingCDTimer = 0;
-                    weapon.isCooling = false;
+                    if (weapon.coolingCDTimer <= 0)
+                    {
+                        weapon.coolingCDTimer = 0;
+                        weapon.isCooling = false;
+                    }
+                    else
+                        weapon.coolingCDTimer -= Time.deltaTime;
                 }
-                else
+                //If the weapon is recharging, drecrease the cooldown timer faster until it is less than or equal to 0
+                else if (weapon.isCooling && weapon.recharging)
+                {
+                    if (weapon.coolingCDTimer <= 0)
+                    {
+                        weapon.coolingCDTimer = 0;
+                        weapon.isCooling = false;
+                    }
+                    else
+                        weapon.coolingCDTimer -= Time.deltaTime * weapon.rechargingSpeed;
+                }
+                //If the weapon is not cooling but its cooldownCDTimer is greater tahn 0, decrease teh cooldown timer by Time.deltaTime
+                else if (weapon.coolingCDTimer > 0)
                     weapon.coolingCDTimer -= Time.deltaTime;
-            }
-            //If the weapon is recharging, drecrease the cooldown timer faster until it is less than or equal to 0
-            else if (weapon.isCooling && weapon.recharging)
-            {
-                if (weapon.coolingCDTimer <= 0)
-                {
-                    weapon.coolingCDTimer = 0;
-                    weapon.isCooling = false;
-                }
-                else
-                    weapon.coolingCDTimer -= Time.deltaTime * weapon.rechargingSpeed;
-            }
-            //If the weapon is not cooling but its cooldownCDTimer is greater tahn 0, decrease teh cooldown timer by Time.deltaTime
-            else if (weapon.coolingCDTimer > 0)
-                weapon.coolingCDTimer -= Time.deltaTime;
-            //If the weapon's coolingCDTimer is greater than or equal to the coolingCooldown, set the weapon cooling to true
-            if (weapon.coolingCDTimer >= weapon.coolingCooldown)
-                weapon.isCooling = true;
+                //If the weapon's coolingCDTimer is greater than or equal to the coolingCooldown, set the weapon cooling to true
+                if (weapon.coolingCDTimer >= weapon.coolingCooldown)
+                    weapon.isCooling = true;
 
-            //If the weapon has shot, increase the rate of fire cooldown until it is greater tahn or equal to the cooldown length
-            if (weapon.hasShot)
-            {
-                if (weapon.rateOfFireCDTimer >= weapon.rateOfFire)
+                //If the weapon has shot, increase the rate of fire cooldown until it is greater tahn or equal to the cooldown length
+                if (weapon.hasShot)
                 {
-                    weapon.rateOfFireCDTimer = 0;
-                    weapon.hasShot = false;
+                    if (weapon.rateOfFireCDTimer >= weapon.rateOfFire)
+                    {
+                        weapon.rateOfFireCDTimer = 0;
+                        weapon.hasShot = false;
+                    }
+                    else
+                        weapon.rateOfFireCDTimer += Time.deltaTime;
                 }
-                else
-                    weapon.rateOfFireCDTimer += Time.deltaTime;
             }
-        }
-        foreach (Weapons weapon in weaponList.tertiaryWeapons)
-        {
-            //If the weapon is cooling and is not recharging, decrease the cooldown timer until it is less than or equal to 0
-            if (weapon.isCooling && !weapon.recharging)
+        if (weaponList.tertiaryWeapons != null)
+            foreach (Weapons weapon in weaponList.tertiaryWeapons)
             {
-                if (weapon.coolingCDTimer <= 0)
+                //If the weapon is cooling and is not recharging, decrease the cooldown timer until it is less than or equal to 0
+                if (weapon.isCooling && !weapon.recharging)
                 {
-                    weapon.coolingCDTimer = 0;
-                    weapon.isCooling = false;
+                    if (weapon.coolingCDTimer <= 0)
+                    {
+                        weapon.coolingCDTimer = 0;
+                        weapon.isCooling = false;
+                    }
+                    else
+                        weapon.coolingCDTimer -= Time.deltaTime;
                 }
-                else
+                //If the weapon is recharging, drecrease the cooldown timer faster until it is less than or equal to 0
+                else if (weapon.isCooling && weapon.recharging)
+                {
+                    if (weapon.coolingCDTimer <= 0)
+                    {
+                        weapon.coolingCDTimer = 0;
+                        weapon.isCooling = false;
+                    }
+                    else
+                        weapon.coolingCDTimer -= Time.deltaTime * weapon.rechargingSpeed;
+                }
+                //If the weapon is not cooling but its cooldownCDTimer is greater tahn 0, decrease teh cooldown timer by Time.deltaTime
+                else if (weapon.coolingCDTimer > 0)
                     weapon.coolingCDTimer -= Time.deltaTime;
-            }
-            //If the weapon is recharging, drecrease the cooldown timer faster until it is less than or equal to 0
-            else if (weapon.isCooling && weapon.recharging)
-            {
-                if (weapon.coolingCDTimer <= 0)
-                {
-                    weapon.coolingCDTimer = 0;
-                    weapon.isCooling = false;
-                }
-                else
-                    weapon.coolingCDTimer -= Time.deltaTime * weapon.rechargingSpeed;
-            }
-            //If the weapon is not cooling but its cooldownCDTimer is greater tahn 0, decrease teh cooldown timer by Time.deltaTime
-            else if (weapon.coolingCDTimer > 0)
-                weapon.coolingCDTimer -= Time.deltaTime;
-            //If the weapon's coolingCDTimer is greater than or equal to the coolingCooldown, set the weapon cooling to true
-            if (weapon.coolingCDTimer >= weapon.coolingCooldown)
-                weapon.isCooling = true;
+                //If the weapon's coolingCDTimer is greater than or equal to the coolingCooldown, set the weapon cooling to true
+                if (weapon.coolingCDTimer >= weapon.coolingCooldown)
+                    weapon.isCooling = true;
 
-            //If the weapon has shot, increase the rate of fire cooldown until it is greater tahn or equal to the cooldown length
-            if (weapon.hasShot)
-            {
-                if (weapon.rateOfFireCDTimer >= weapon.rateOfFire)
+                //If the weapon has shot, increase the rate of fire cooldown until it is greater tahn or equal to the cooldown length
+                if (weapon.hasShot)
                 {
-                    weapon.rateOfFireCDTimer = 0;
-                    weapon.hasShot = false;
+                    if (weapon.rateOfFireCDTimer >= weapon.rateOfFire)
+                    {
+                        weapon.rateOfFireCDTimer = 0;
+                        weapon.hasShot = false;
+                    }
+                    else
+                        weapon.rateOfFireCDTimer += Time.deltaTime;
                 }
-                else
-                    weapon.rateOfFireCDTimer += Time.deltaTime;
             }
-        }
     }
 }
 
@@ -204,6 +207,25 @@ public class WeaponClasses
     public Weapons[] primaryWeapons;
     public Weapons[] secondaryWeapons;
     public Weapons[] tertiaryWeapons;
+
+    public WeaponClasses(WeaponClasses other)
+    {
+        primaryWeapons = new Weapons[other.primaryWeapons.Length];
+        secondaryWeapons = new Weapons[other.secondaryWeapons.Length];
+        tertiaryWeapons = new Weapons[other.tertiaryWeapons.Length];
+        for (int i = 0; i < other.primaryWeapons.Length; i++)
+        {
+            this.primaryWeapons[i] = new Weapons(other.primaryWeapons[i]);
+        }
+        for (int i = 0; i < other.secondaryWeapons.Length; i++)
+        {
+            this.secondaryWeapons[i] = new Weapons(other.secondaryWeapons[i]);
+        }
+        for (int i = 0; i < other.tertiaryWeapons.Length; i++)
+        {
+            this.tertiaryWeapons[i] = new Weapons(other.tertiaryWeapons[i]);
+        }
+    }
 }
 //Weapon data
 [Serializable]
@@ -227,17 +249,44 @@ public class Weapons
     public float bulletSpeed;
     public float rateOfFire;
     public float recoilAmount;
+    public float adsSpeed;
 
     //Rate of fire variables
-    [HideInInspector] public bool hasShot;
-    [HideInInspector] public float rateOfFireCDTimer;
+    public bool hasShot;
+    public float rateOfFireCDTimer;
 
     //Cooling variables
-    [HideInInspector] public bool isCooling;
-    [HideInInspector] public bool recharging;
+    public bool isCooling;
+    public bool recharging;
     [Header("Cooling Variables")]
     public float coolingCooldown;
-    [HideInInspector] public float coolingCDTimer;
+    public float coolingCDTimer;
     public float coolingCDIncrease;
     public float rechargingSpeed = 1;
+
+    public Weapons(Weapons other)
+    {
+        this.weaponName = other.weaponName;
+        this.weaponPrefab = other.weaponPrefab;
+        this.bulletPrefab = other.bulletPrefab;
+        this.unlocked = other.unlocked;
+
+        this.fireMode = other.fireMode;
+        this.damage = other.damage;
+        this.spreadRadius = other.spreadRadius;
+        this.bulletCount = other.bulletCount;
+        this.bulletSpeed = other.bulletSpeed;
+        this.rateOfFire = other.rateOfFire;
+        this.recoilAmount = other.recoilAmount;
+        this.adsSpeed = other.adsSpeed;
+
+        this.hasShot = other.hasShot;
+        this.rateOfFireCDTimer = other.rateOfFireCDTimer;
+
+        this.isCooling = other.isCooling;
+        this.recharging = other.recharging;
+        this.coolingCooldown = other.coolingCooldown;
+        this.coolingCDTimer = other.coolingCDTimer;
+        this.rechargingSpeed = other.rechargingSpeed;
+    }
 }

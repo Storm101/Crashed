d4fdc0f0e64c6a4ece4527cfa4acd2c9be6269cc
Storm101 <trimoore5@gmail.com>
2022-10-12 19:18:19 +1100

    Update ShipRepairs.cs

--- Assets/Scripts/Ship/ShipRepairs.cs
+++ Assets/Scripts/Ship/ShipRepairs.cs
@@ -54,6 +54,7 @@ public class ShipRepairs : MonoBehaviour
 
             if (waveStarted == false) {
                 StartCoroutine(SpawnEnemy());
+                transform.GetChild(0).GetComponent<ShipHealth>().health = transform.GetChild(0).GetComponent<ShipHealth>().maxHealth;
                 health.health = health.maxHealth;
                 health.healthVisible = true;
                 waveStarted = true;
@@ -67,6 +68,7 @@ public class ShipRepairs : MonoBehaviour
                 if (totalRepairTime <= 0 && !gameManager.alertActive)
                 {
                     gameManager.GetComponent<AlertBox>().AlertPopup("Ship successfully repaired! \n Press \"e\" on the ship to take off!");
+                    gameManager.shipRepaired = true;
                 }
             }
         }

a7a23094ae1789e6c0c471d19f377f856fc5d4dd
Storm101 <trimoore5@gmail.com>
2022-10-12 19:53:27 +1100

    Doors

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -5,26 +5,11 @@ using UnityEngine.Animations;
 
 public class Door : MonoBehaviour
 {
-    [Tooltip("What enemy prefabs to spawn")]
-    public GameObject enemyPrefab;
-
-    [Tooltip("Where to spawn the enemies")]
-    public GameObject[] enemySpawnPoints;
-
-    [Tooltip("How many waves of how many enemies")]
-    public int[] waves;
-
-    private bool inWave;
-    private MeshRenderer m_Renderer;
-    private BoxCollider m_Collider;
-    private BoxCollider m_Trigger;
-
     public bool locked;
     public GameObject lockIndicator;
     private MeshRenderer lockIndicatorColour;
     private bool up;
 
-    private int CurrentWave = -1;
     private Animator animator;
 
     public GameObject Player;
@@ -34,69 +19,35 @@ public class Door : MonoBehaviour
 
     public float doorDistance;
 
-    private void Start() {
-        BoxCollider[] boxColliders = GetComponents<BoxCollider>();
-        if (boxColliders.Length != 2) {
-            Debug.LogError("Wrong amount of colliders");
-        }
-        if (boxColliders[0].isTrigger) {
-            m_Trigger = boxColliders[0];
-            m_Collider = boxColliders[1];
-        }
-        else {
-            m_Trigger = boxColliders[1];
-            m_Collider = boxColliders[0];
-        }
-        m_Renderer = GetComponent<MeshRenderer>();
-        //m_Renderer.enabled = false;
-        m_Trigger.enabled = true;
-        inWave = false;
+    private void Start()
+    {
         animator = GetComponent<Animator>();
         lockIndicatorColour = lockIndicator.GetComponent<MeshRenderer>();
         lockIndicatorColour.material = unlockedMat;
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player") {
-            inWave = true;
-            //m_Renderer.enabled = true;
-            m_Trigger.enabled = false;
-            //animator.Play("Base Layer.Door", 0, -1);
-            locked = true;
-        }
-    }
-
-    private void LateUpdate() {
-        if (inWave) {
-            if (GameManager.Instance.EnemiesLeft == 0) {
-                if (CurrentWave == waves.Length-1) {
-                    //m_Renderer.enabled = false;
-                    locked = false;
-                    inWave = false;
-                }
-                else {
-                    CurrentWave++;
-                    for (int i = 0; i < waves[CurrentWave]; i++) {
-                        Instantiate(enemyPrefab, enemySpawnPoints[Random.Range(0, enemySpawnPoints.Length - 1)].transform.position, enemySpawnPoints[0].transform.rotation);
-                    }
-                }
-            }
-        }
-        if (locked) {
-            if (up == true) {
+    private void LateUpdate()
+    {
+        if (locked)
+        {
+            if (up == true)
+            {
                 animator.SetBool("OpenDoor", false);
                 up = false;
             }
             lockIndicatorColour.material = lockedMat;
         }
-        else {
+        else
+        {
             float i = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(Player.transform.position.x, Player.transform.position.z));
-            if (!up && i < doorDistance) {
+            if (!up && i < doorDistance)
+            {
                 //animator.Play("Base Layer.DoorReverse", 0, -1);
                 up = true;
                 animator.SetBool("OpenDoor", true);
             }
-            else if (up && i > doorDistance + 0.1f) {
+            else if (up && i > doorDistance + 0.1f)
+            {
                 up = false;
                 //animator.Play("Base Layer.Door", 0, -1);
                 animator.SetBool("OpenDoor", false);

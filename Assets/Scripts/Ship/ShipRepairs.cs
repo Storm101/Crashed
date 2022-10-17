5730d22bd6ad8c7cd4060c518cb809fe6fb70d68
Storm101 <trimoore5@gmail.com>
2022-10-17 17:22:42 +1100

    Fixed bug with ship health reference

--- Assets/Scripts/Ship/ShipRepairs.cs
+++ Assets/Scripts/Ship/ShipRepairs.cs
@@ -36,7 +36,7 @@ public class ShipRepairs : MonoBehaviour
     {
         gameManager = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();
 
-        health = GetComponent<ShipHealth>();
+        health = transform.GetChild(0).GetComponent<ShipHealth>();
     }
 
     private void Update() {

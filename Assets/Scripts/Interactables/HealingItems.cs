a5521d28e441390337d357ec1e0c3bcead045a57
Storm101 <trimoore5@gmail.com>
2022-10-26 20:30:47 +1100

    Added health orbs bobbing up and down

--- Assets/Scripts/Interactables/HealingItems.cs
+++ Assets/Scripts/Interactables/HealingItems.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class HealingItems : MonoBehaviour
 {
+    public float bobTime;
+    private bool bobUp = true;
+    private void Update()
+    {
+        if (bobTime < 0.5f && bobUp)
+            bobTime += Time.deltaTime;
+        else if (bobTime >= 0.5f && bobUp)
+            bobUp = false;
+
+        if (bobTime > -0.5f && !bobUp)
+            bobTime -= Time.deltaTime;
+        else if (bobTime <= -0.5f)
+            bobUp = true;
+
+        transform.Translate(new Vector3(0, bobTime, 0) * Time.deltaTime);
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             if (other.GetComponent<PlayerHealth>().health != other.GetComponent<PlayerHealth>().maxHealth) {
